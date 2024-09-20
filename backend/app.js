c91872b2fde9be2b89a9aa41b8b88408bf89e01f
ayushm98 <ayushkumarmalik10@gmail.com>
2024-09-19 23:55:03 -0400

    cahnges

--- backend/app.js
+++ backend/app.js
@@ -30,13 +30,14 @@ mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/soccer-wealth-i
 */
 
 // Connect to MongoDB using the URI from environment variables
-mongoose.connect(process.env.MONGODB_URI)
-  .then(() => {
-    console.log("MongoDB connected...");
-  })
-  .catch((err) => {
-    console.error("Error connecting to MongoDB", err);
-  });
+mongoose.connect(process.env.MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log("MongoDB connected...");
+}).catch((err) => {
+  console.error("Error connecting to MongoDB", err);
+});
 
 
 // Test route
